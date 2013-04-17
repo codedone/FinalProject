4b864ab4de4fd2ffac6e99112de96ab2f6ed2018
Travis Lamb <travis.t.lamb@gmail.com>
2013-04-17 17:09:02 -0500

    Seperated authentication from tweeting. Added XML writing. Removed old file

---
+++ TwitterTest/Authenticate.py
@@ -0,0 +1,19 @@
+
+import sys
+import os
+from twitter import *
+
+def getTwitter():
+    APP_NAME = "CSCE315 FinalProject"
+    CONSUMER_KEY = "laLkGfnAEQZ8OmoCFFBQ"
+    CONSUMER_SECRET = "lEVBBe5vUGwFtuZHd4nAkjeoo1HwEsT7cPHKFcpxsU"
+
+    MY_TWITTER_CREDS = os.path.expanduser('oauth_creds')
+
+    if not os.path.exists(MY_TWITTER_CREDS):
+        oauth_dance( APP_NAME, CONSUMER_KEY, CONSUMER_SECRET, MY_TWITTER_CREDS)
+
+    oauth_token, oauth_secret = read_token_file(MY_TWITTER_CREDS)
+    twitter = Twitter(auth=OAuth(oauth_token, oauth_secret, CONSUMER_KEY, CONSUMER_SECRET))
+
+    return twitter
--- TwitterTest/Tweet.py
+++ TwitterTest/Tweet.py
@@ -1,7 +1,5 @@
 
-import sys
-import os
-from twitter import *
+from Authenticate import *
 
 if len(sys.argv) is not 2:
     print("Please post your status as a string in quotes.")
@@ -9,12 +7,6 @@ if len(sys.argv) is not 2:
 
 statusUpdate = sys.argv[1]
 
-MY_TWITTER_CREDS = os.path.expanduser('oauth_creds')
+twitter = getTwitter()
 
-if not os.path.exists(MY_TWITTER_CREDS):
-	oauth_dance("CSCE315 FinalProject", "laLkGfnAEQZ8OmoCFFBQ", "lEVBBe5vUGwFtuZHd4nAkjeoo1HwEsT7cPHKFcpxsU", MY_TWITTER_CREDS)
-
-oauth_token, oauth_secret = read_token_file(MY_TWITTER_CREDS)
-twitter = Twitter(auth=OAuth(oauth_token, oauth_secret, "laLkGfnAEQZ8OmoCFFBQ", "lEVBBe5vUGwFtuZHd4nAkjeoo1HwEsT7cPHKFcpxsU"))
 twitter.statuses.update(status = statusUpdate)
-
