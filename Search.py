03ef0e5380b4b51861c58b83668e1e6bded58f72
Jacob Pokora <jtpokora@gmail.com>
2013-05-06 16:45:40 -0500

    Read Reddit XML, added date to twitter XML reader

--- Search.py
+++ Search.py
@@ -1,6 +1,39 @@
 import Post
 import string
 import re
+import random
+
+def InitializeRedditFromXML():
+    redditList = []
+
+    post = ''
+    user = ''
+    date = '1/1/2013'
+    URL = ''
+
+    f = open('RedditAPI/unsorted_posts.xml')
+
+    for line in f:
+        line = ''.join(line.split())
+        if line == '<post>':
+            continue
+        if '<author>' in line:
+            user = line
+            user = user.replace('<author>','').replace('</author>','')
+        if '<title>' in line:
+            post = line
+            user = user.replace('<title>','').replace('</title>','')
+        if '<url>' in line:
+            URL = line
+            URL = URL.replace('<url>','').replace('</url>','')
+        if line == '</post>':
+            p = Post.RedditPost(post, user, date, 0, 0, URL)
+            redditList.append(p)
+            post = ''
+            user = ''
+            url = ''
+
+    return redditList
 
 def InitializeTwitterFromXML():
     tweetList = []
@@ -26,15 +59,46 @@ def InitializeTwitterFromXML():
                 line = f.readline()
                 user += line
             user = user.replace("<author>","").replace("</author>","")
+        if "<date>" in line:
+            date = line
+            date = date.replace("<date>","").replace("</date>","")
         if line == "</tweet>":
-            p = Post.TwitterPost(post,user,date,0)
+            p = Post.TwitterPost(post,user,date)
             tweetList.append(p)
             post = ''
             user = ''
+            date = '1/1/2001'
 
     return tweetList
 
+def SearchFrontPage(param):
+    TwitterList = InitializeTwitterFromXML()
+    RedditList = InitializeRedditFromXML()
+    RelevantPosts = []
+
+    for post in TwitterList:
+        line = post.title.split()
+        for word in line:
+            if param.lower() in word.lower():
+                RelevantPosts.append(post)
+                break
+
+    for post in RedditList:
+        line = post.title.split()
+        for word in line:
+            if param.lower() in word.lower():
+                RelevantPosts.append(post)
+                break
+
+    random.shuffle(RelevantPosts)
+
+    return RelevantPosts
 
 def FrontPage():
     PostList = InitializeTwitterFromXML()
-    return PostList #this is temporary for Matt to do his UI stuff
+    PostList2 = InitializeRedditFromXML()
+
+    for post in PostList2:
+        PostList.append(post)
+
+    return PostList
