493afdf7d6347fc13d69bca3da227117da9e3ddf
Jacob Pokora <jtpokora@gmail.com>
2013-05-06 22:44:32 -0500

    Commented, added refresh feature to reddit

--- Search.py
+++ Search.py
@@ -2,6 +2,7 @@ import Post
 import string
 import re
 import random
+import PostsToXML
 
 def InitializeRedditFromXML():
     redditList = []
@@ -11,11 +12,12 @@ def InitializeRedditFromXML():
     date = '1/1/2013'
     URL = ''
 
-    f = open('RedditAPI/unsorted_posts.xml')
+    PostsToXML #update XML to retrieve latest posts
+    f = open('RedditAPI/unsorted_posts.xml') #open file for reading
 
-    for line in f:
+    for line in f: 
         line = ' '.join(line.split())
-        if line == '<post>':
+        if line == '<post>': #break out XML file based on tag
             continue
         if '<author>' in line:
             user = line
@@ -27,7 +29,7 @@ def InitializeRedditFromXML():
             URL = line
             URL = URL.replace('<url>','').replace('</url>','')
         if line == '</post>':
-            p = Post.RedditPost(post, user, date, 0, 0, URL)
+            p = Post.RedditPost(post, user, date, 0, 0, URL) #take parsed information and create a Post object
             redditList.append(p)
             post = ''
             user = ''
@@ -39,31 +41,35 @@ def InitializeTwitterFromXML():
     tweetList = []
     post = ''
     user = ''
-    date = '1/1/2013' #needs to be read in once included in xml
+    date = '1/1/2013' 
 
-    f = open('unsorted_tweets.xml')
+    f = open('TwitterAPI/unsorted_tweets.xml') #open XMl file for reading
 
-    for line in f:
+    while True:
+        line = f.readline()
+        if not line: break
         line = ' '.join(line.split())
-        if line == '<tweet>':
+        if line == '<tweet>':  #parse XML file based on tags
             continue
         if "<post>" in line:
             post+=line
             while "</post>" not in line:
                 line = f.readline()
+                post+=' '
                 post+=line
             post = post.replace("<post>","").replace("</post>","")
         if "<author>" in line:
             user += line
             while "</author>" not in line:
                 line = f.readline()
+                user += ' '
                 user += line
             user = user.replace("<author>","").replace("</author>","")
         if "<date>" in line:
             date = line
             date = date.replace("<date>","").replace("</date>","")
         if line == "</tweet>":
-            p = Post.TwitterPost(post,user,date)
+            p = Post.TwitterPost(post,user,date) #create object from parsed data
             tweetList.append(p)
             post = ''
             user = ''
@@ -76,16 +82,16 @@ def SearchFrontPage(param):
     RedditList = InitializeRedditFromXML()
     RelevantPosts = []
 
-    for post in TwitterList:
+    for post in TwitterList:  
         line = post.title.split()
-        for word in line:
+        for word in line: #traverse through all words and find posts that match search parameters
             if param.lower() in word.lower():
                 RelevantPosts.append(post)
                 break
 
     for post in RedditList:
         line = post.title.split()
-        for word in line:
+        for word in line: #traverse through all words and find posts that match search parameters
             if param.lower() in word.lower():
                 RelevantPosts.append(post)
                 break
@@ -98,7 +104,7 @@ def FrontPage():
     PostList = InitializeTwitterFromXML()
     PostList2 = InitializeRedditFromXML()
 
-    for post in PostList2:
+    for post in PostList2: #combines Reddit and Twitter Posts
         PostList.append(post)
     
     random.shuffle(PostList)
