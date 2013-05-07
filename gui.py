842f0561206c518b5fb27ee95a212ce29dd7a567
Matthew Dillard <mld2443@tamu.edu>
2013-05-06 20:22:06 -0500

    editted GUI to take current posts

--- gui.py
+++ gui.py
@@ -3,6 +3,7 @@ from PIL import Image, ImageTk
 from Tkinter import Tk, Text, Label, RIGHT, LEFT, BOTH, RAISED, TOP, BOTTOM, X, Y, W
 from ttk import Frame, Style
 import webbrowser as web
+import Search
 
 class GUI(Frame):
 
@@ -18,30 +19,31 @@ class GUI(Frame):
 		
 		self.pack(fill=BOTH, expand=1)
 		
-		posts = [["Reddit", "http://upload.wikimedia.org/wikipedia/en/a/af/Maine_Moose_Logo.png", "DrPresley", "Way funnier than the movie!", "http://www.youtube.com/watch?v=LJQ-LZYAMBQ", "today"], \
-				 ["Reddit", "http://upload.wikimedia.org/wikipedia/en/a/af/Maine_Moose_Logo.png", "NotACat", "there's a reason it's called the best twitter bot", "https://twitter.com/Horse_ebooks", "today"], \
-				 ["Reddit", "http://upload.wikimedia.org/wikipedia/en/a/af/Maine_Moose_Logo.png", "very helpful if you know what you are doing...", "http://www.reddit.com/r/Python/", "yesterday"]]
+		posts = Search.FrontPage()
 		
 		for post in posts:
 			frame = Frame(self)
 			frame.pack(side=TOP, fill=X)
 			
-			file = cStringIO.StringIO(urllib.urlopen(post[1]).read())
-			raw = Image.open(file)
-			img = ImageTk.PhotoImage(raw)
-			thumb = Label(frame, image=img)
-			thumb.image = img
-			thumb.bind("<1>", lambda event, url=post[1]: web.open_new(url))
-			thumb.pack(side=LEFT)
+			#file = cStringIO.StringIO(urllib.urlopen("location").read())
+			if (post._class_._name_ == "RedditPost")
+				raw = Image.open("reddit.png").resize((40,40), Image.ANTIALIAS)
+			else
+				raw = Image.open("twitter.png").resize((40,40), Image.ANTIALIAS)
+			#img = ImageTk.PhotoImage(raw)
+			#thumb = Label(frame, image=img)
+			#thumb.image = img
+			#thumb.bind("<1>", lambda event, url=post[1]: web.open_new(url))
+			#thumb.pack(side=LEFT)
 			
 			body = Frame(frame)
 			body.pack(fill=BOTH)
 			
-			title = Label(body, text=post[3], foreground="#0000dd")
-			title.bind("<1>", lambda event, url=post[4]: web.open_new(url))
+			title = Label(body, text=post.title, foreground="#0000dd")
+			#title.bind("<1>", lambda event, url=post[4]: web.open_new(url))
 			title.pack(anchor=W)
 			
-			details = Label(body, text=post[5]+" by "+post[2])
+			details = Label(body, text=post.date+" by "+post.user)
 			details.pack(anchor=W)
 
 
