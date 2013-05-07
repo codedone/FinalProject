43cb43f92171b7ca5f0dc966b20e71f39a1bfc2d
Matthew Dillard <mld2443@tamu.edu>
2013-05-06 23:04:20 -0500

    added login window and notes

--- gui.py
+++ gui.py
@@ -4,11 +4,18 @@ from Tkinter import Tk, Text, Label, Message, Entry, Button, RIGHT, LEFT, BOTH, 
 from ttk import Frame, Style
 import webbrowser as web
 import Search
+import reddituserpass
+
 
 class GUI(Frame):
 	
-	def __init__(self, parent):
+	posts = []
+	credentials = []
+	
+	def __init__(self, parent, _posts):
 		Frame.__init__(self, parent)
+		
+		self.posts = _posts
 		
 		self.parent = parent
 		self.parent.title("Media Portal")
@@ -19,57 +26,76 @@ class GUI(Frame):
 		
 		self.pack(fill=BOTH, expand=1)
 		
+		#create special frame for buttons at the top
 		frame = Frame(self)
 		frame.pack(fill=X)
 		
 		search = Entry(frame)
 		
 		def callback():
-			print search.get()
-
+			#create new window
+			main(Search.SearchFrontPage(search.get()))
+		
 		b = Button(frame, text="Search", width=5, command=callback)
 		b.pack(side=RIGHT)
 		search.pack(side=RIGHT)
 		
-		posts = Search.FrontPage()
+		def login():
+			#change login credentials
+			self.credentials = reddituserpass.main()
+			self.parent.destroy()
 		
-		for post in posts:
+		login = Button(frame, text="Login", width=5, command=login)
+		login.pack(side=LEFT)
+		
+		self.drawWindow()
+
+	def drawWindow(self):
+		#cache the images to display
+		self.redditraw = Image.open("reddit.png").resize((40,40), Image.ANTIALIAS)
+		self.redditimg = ImageTk.PhotoImage(self.redditraw)
+		
+		self.twitterraw = Image.open("twitter.png").resize((40,40), Image.ANTIALIAS)
+		self.twitterimg = ImageTk.PhotoImage(self.twitterraw)
+		
+		for post in self.posts:
 			frame = Frame(self)
 			frame.pack(side=TOP, fill=X)
 			
+			#this was for using web images
 			#file = cStringIO.StringIO(urllib.urlopen("location").read())
 			
+			#draw 
 			if post.type == "RedditPost":
-				raw = Image.open("reddit.png").resize((40,40), Image.ANTIALIAS)
-				img = ImageTk.PhotoImage(raw)
-				thumb = Label(frame, image=img)
-				thumb.image = img
-				#thumb.bind("<1>", lambda event, url=post[1]: web.open_new(url))
-				thumb.pack(side=LEFT)
+				reddit = Label(frame, image=self.redditimg)
+				reddit.image = self.redditimg
+				#reddit.bind("<1>", lambda event, url=post[1]: web.open_new(url))
+				reddit.pack(side=LEFT)
 			else:
-				raw = Image.open("twitter.png").resize((40,40), Image.ANTIALIAS)
-				img = ImageTk.PhotoImage(raw)
-				thumb = Label(frame, image=img)
-				thumb.image = img
-				#thumb.bind("<1>", lambda event, url=post[1]: web.open_new(url))
-				thumb.pack(side=LEFT)
+				twitter = Label(frame, image=self.twitterimg)
+				twitter.image = self.twitterimg
+				#twitter.bind("<1>", lambda event, url=post[1]: web.open_new(url))
+				twitter.pack(side=LEFT)
 			
 			body = Frame(frame)
 			body.pack(fill=BOTH)
 			
+			#the actual post body
 			title = Message(body, text=post.title, foreground="#0000dd", justify=LEFT, width=550)
 			#title.bind("<1>", lambda event, url=post[4]: web.open_new(url))
 			title.pack(anchor=W)
 			
+			#date and author
 			details = Label(body, text=post.date+" by "+post.user)
 			details.pack(anchor=W)
 
 
-def main():
+def main(posts = Search.FrontPage()):
 	
+	#main window
 	root = Tk()
 	root.geometry("600x300+300+300")
-	app = GUI(root)
+	app = GUI(root,posts)
 	root.mainloop()
 
 if __name__ == '__main__':
--- reddituserpass.py
+++ reddituserpass.py
@@ -1,25 +1,47 @@
 from Tkinter import *
 
-root = Tk()
 
-def makeentry(parent, caption, width=None, **options):
-	frame = Frame(parent)
-	Label(frame, text=caption).pack(side=LEFT)
-	entry = Entry(frame, **options)
-	if width:
-		entry.config(width=width)
-	entry.pack(side=LEFT)
-	frame.pack(fill=X)
-	return entry
+class Login(Frame):
+	
+	data = []
+	
+	def __init__(self, parent):
+		Frame.__init__(self, parent)
+		
+		self.parent = parent
+		self.parent.title("Login")
+		
+		self.initUI()
 
-user = makeentry(root, "User name:", 10)
-password = makeentry(root, "Password:", 10, show="*")
+	def initUI(self):
+		self.pack(fill=BOTH, expand=1)
+		
+		def makeentry(parent, caption, width=None, **options):
+			frame = Frame(parent)
+			Label(frame, text=caption).pack(side=LEFT)
+			entry = Entry(frame, **options)
+			if width:
+				entry.config(width=width)
+			entry.pack(side=LEFT)
+			frame.pack(fill=X)
+			return entry
 
-def callback():
-	combo = [user.get(), password.get()]
-	print combo
+		user = makeentry(self, "User name:", 10)
+		password = makeentry(self, "Password:", 10, show="*")
 
-b = Button(root, text="Login", width=10, command=callback)
-b.pack()
+		def callback():
+			self.data = [user.get(), password.get()]
+			self.parent.destroy()
 
-mainloop()
+		b = Button(self, text="Login", width=10, command=callback)
+		b.pack()
+
+def main():
+	root = Tk()
+	app = Login(root)
+	root.mainloop()
+	return app.data
+
+if __name__ == '__main__':
+	main()
+
